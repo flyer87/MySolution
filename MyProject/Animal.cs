0f87e4c6d9a3ac3b5d1d816bdbbf13658d5f8369
Omer Yumer <y.yumer@gmail.com>
2017-08-28 13:49:16 +0200

    Added: _age, _name to the classes Animal and Person

---
+++ MyProject/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    class Animal
+    {
+        private int _age;
+        private string _name;
+    }
+}
