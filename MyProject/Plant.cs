f8d593bec65bd134ea14b889a548b1a315327eb0
Omer Yumer <y.yumer@gmail.com>
2017-08-29 12:34:49 +0200

    Added: Properties Age + Name in Plant.cs

--- MyProject/Plant.cs
+++ MyProject/Plant.cs
@@ -9,6 +9,32 @@ namespace MyProject
     public class Plant
     {
         private int _age;
-        private string _name;         
+        private string _name;
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+
+            set
+            {
+                _age = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
     }
 }
