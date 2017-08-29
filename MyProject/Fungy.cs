787fbc6a6a98da2a6552c9aaeb27503ab3b2d914
Omer Yumer <y.yumer@gmail.com>
2017-08-29 14:48:04 +0200

    Added: Fungy.cs with properties Age + Name

---
+++ MyProject/Fungy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    class Fungy
+    {
+        private int _age;
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+
+    }
+}
