48544dfa1c7ef6fdc93d6fdfbbb31787a9381a38
Omer Yumer <y.yumer@gmail.com>
2017-08-29 12:32:42 +0200

    Added: Plant.cs + _age, _name at the same time

---
+++ MyProject/Plant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    public class Plant
+    {
+        private int _age;
+        private string _name;         
+    }
+}
